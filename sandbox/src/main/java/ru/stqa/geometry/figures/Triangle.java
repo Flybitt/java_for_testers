714eb73e2f5ddda79f7e23e25110ccd7b4a2697d
Максим Румянцев <rumyantsev15@icloud.com>
2025-02-09 14:07:22 +0700

    Задание №3

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -6,6 +6,15 @@ public record Triangle(
         double c
 ) {
 
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        if ((a + b) < c || (a + c) < b || (b + c) < a) {
+            throw new IllegalArgumentException("Sum of two sides must not be less than third side");
+        }
+    }
+
     public double perimeter() {
         return this.a + this.b + this.c;
     }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -14,4 +14,36 @@ public class TriangleTests {
     void calcArea() {
         Assertions.assertEquals(6, new Triangle(3.0,4.0,5.0).area());
     }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-3,4,6);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+    }
+
+    @Test
+    void sumTwoSidesMustNotBeLessThird() {
+        try {
+            new Triangle(1,3,5);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+        try {
+            new Triangle(3, 1, 5);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+        try {
+            new Triangle(5, 1, 3);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+    }
 }
