177dbdc5836646a648876bc40cc883735c79553d
Максим Румянцев <rumyantsev15@icloud.com>
2025-06-08 12:29:52 +0700

    add soap method

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -22,9 +22,14 @@ public class ApplicationManager {
     private JamesCliHelper jamesCliHelper;
 
     private MailHelper mailHelper;
+
     private RegistrationHelper userRegHelper;
+
     private JamesApiHelper jamesApiHelper;
+
     private RestApiHelper restApiHelper;
+
+    private SoapApiHelper soapApiHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -98,6 +103,14 @@ public class ApplicationManager {
             restApiHelper = new RestApiHelper(this);
         }
         return restApiHelper;
+
+    }
+
+    public SoapApiHelper soap() {
+        if (soapApiHelper == null) {
+            soapApiHelper = new SoapApiHelper(this);
+        }
+        return soapApiHelper;
 
     }
 }
